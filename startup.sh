360c3f591b401b68de2f6a9ecd2a5ae8720cfdb2
zodius <zodius.chen@gmail.com>
2023-05-03 15:03:49 +0800

    feat: :sparkles: Add Penta

--- startup.sh
+++ startup.sh
@@ -2,3 +2,5 @@
 
 docker compose -f ./traefik/docker-compose.yml --env-file .env up -d
 docker compose -f ./ip.echo/docker-compose.yml --env-file .env up -d
+docker compose -f ./static/docker-compose.yml --env-file .env up -d
+docker compose -f ./penta/docker-compose.yml --env-file .env up -d
--- teardown.sh
+++ teardown.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
-docker compose -f ./traefik/docker-compose.yml down
-docker compose -f ./ip.echo/docker-compose.yml down
+docker compose -f ./traefik/docker-compose.yml --env-file .env down
+docker compose -f ./ip.echo/docker-compose.yml --env-file .env down
+docker compose -f ./static/docker-compose.yml --env-file .env down
+docker compose -f ./penta/docker-compose.yml --env-file .env down
